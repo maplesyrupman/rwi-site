29c046157796d8f3f5b64f7f2b4c2e3873c2610a
smudesign <urwinsamantha@gmail.com>
2022-06-25 22:44:33 -0400

    button styles change based on prop

--- components/Buttons/Link.jsx
+++ components/Buttons/Link.jsx
@@ -3,13 +3,24 @@ import Link from 'next/link';
 
 export default function Link({ text, href, linkStyle }) {
 
+    switch(linkStyle) {
+        case 'default':
+            linkStyle = 'styles.linkDefault';
+            break;
+        case 'bold':
+            linkStyle = 'styles.linkBold';
+            break;
+        case 'danger':
+            linkStyle = 'styles.linkDanger';
+            break;
+    }
+    
     return (
         <>
         <Link
         href={href}
-        style={`${linkStyle}`}
         >
-            <a>{text}</a>
+            <a style={`${linkStyle}`}>{text}</a>
         </Link>
         </>
     )
