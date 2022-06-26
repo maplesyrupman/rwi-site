bc37a8972cf2a16f4b9f29648aa4a662cecc704d
smudesign <urwinsamantha@gmail.com>
2022-06-26 14:31:27 -0400

    sudo code

--- components/DiscoveryQuestionnaire/ProgressBar.tsx
+++ components/DiscoveryQuestionnaire/ProgressBar.tsx
@@ -1,11 +1,27 @@
 
 import styles from '../../styles/progressBar.module.css'
 
-export default function ProgressBar({sections}) {
+export default function ProgressBar({ sections, percent }) {
 
     return (
-        <div>
-            
+        <div className={styles.progress}>
+            {/* map the number of sections */}
+            {sections.map(section => {
+                return (
+                    <div>
+                        {/* each section has a bar that will "fill" based on the percent value */}
+                        <span 
+                        className={styles.bar}
+                        ></span>
+                        {/* each section will have a circle which is either incomplete or complete */}
+                        <div className={styles.circle}>
+                            
+                        </div>
+                    </div>
+                )
+            })}
+
+
         </div>
     )
 }
