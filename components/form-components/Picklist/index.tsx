bad93539b88d0737edf2ec80ec1387e1d5f1b569
maplesyrupman <maplesyrupman@protonmail.com>
2022-06-26 21:25:40 -0400

    boutta convert picklist to react class

--- components/form-components/Picklist/index.tsx
+++ components/form-components/Picklist/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import styles from './Picklist.module.css'
 
 type Props = {
@@ -14,6 +14,7 @@ type Props = {
 
 export default function Picklist({ id, label, options, other, size, placeholder, multi, change }: Props) {
     const [selected, setSelected] = useState(placeholder)
+    const [dropdownOpen, setDropdownOpen] = useState(false)
 
     let selectStyle: string = ''
     switch (size) {
@@ -26,14 +27,28 @@ export default function Picklist({ id, label, options, other, size, placeholder,
         default:
             selectStyle = styles.lg
     }
-    console.log(selected)
+
+    function handleSelect(e: React.MouseEvent<HTMLDivElement, MouseEvent>):void {
+        const choice = e.target as HTMLDivElement
+        setSelected(choice.dataset.value || placeholder)
+        setDropdownOpen(!dropdownOpen)
+    }
+    
     return (
-        <div className={`${styles.container} ${selectStyle} bgWhite`}>
-            <div className={`txtBlue`}>
+        <div className={`${styles.container} ${selectStyle}`}>
+            <div className={`txtBlue bold`}>
                 {label}
             </div>
-            <div className={styles.select}>
-                <div>
+            <div className={`${styles.select} ${selectStyle} ${dropdownOpen ? styles.selectOpen : ''}`}
+                onClick={function() {
+                    setDropdownOpen(!dropdownOpen)
+                }}
+                onBlur={() => setDropdownOpen(!dropdownOpen)}
+                onFocus={() => console.log('blur!')}
+            >
+                <div className={styles.selected}
+                onFocus={() => console.log('focus!')}
+                >
                     {selected}
                 </div>
                 <div className={styles.carrot}>
@@ -41,28 +56,29 @@ export default function Picklist({ id, label, options, other, size, placeholder,
                         <path d="M15.5607 2.84375L9.19249 9.51042C8.83675 9.83854 8.4259 10 8.01504 10C7.60419 10 7.19434 9.83724 6.88118 9.51172L0.512935 2.84505C0.00960756 2.36979 -0.127698 1.65104 0.120518 1.03125C0.368735 0.411458 0.953852 0 1.6017 0H14.3833C15.0316 0 15.6169 0.40526 15.8654 1.02865C16.1139 1.65203 16.0217 2.36979 15.5607 2.84375Z" fill="#6D6D6D" />
                     </svg>
                 </div>
-                <div className={`${styles.optionsContainer}`}>
-                    <div className={`${styles.options}  ${selectStyle}`}>
-                        {options.map(o => {
-                            return (
-                                <div
-                                    key={o}
-                                    className={`${styles.option}`}
-                                    data-value={o}
-                                >
-                                    {o}
-                                </div>
-                            )
-                        })}
-                        {other && (
+            </div>
+            <div className={`${styles.optionsContainer}`}>
+                <div className={`${styles.options} ${dropdownOpen ? styles.optionsOpen : ''}  ${selectStyle}`}>
+                    {options.map(o => {
+                        return (
                             <div
+                                key={o}
                                 className={`${styles.option}`}
-                                data-value={'other'}
+                                data-value={o}
+                                onClick={e => handleSelect(e)}
                             >
-                                other
+                                {o}
                             </div>
-                        )}
-                    </div>
+                        )
+                    })}
+                    {other && (
+                        <div
+                            className={`${styles.option}`}
+                            data-value={'other'}
+                        >
+                            other
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
