e57e19fe710c2f7a26a740c7e97ba072916ddf20
maplesyrupman <maplesyrupman@protonmail.com>
2022-06-30 16:29:06 -0400

    added validator method on question type

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,25 +1,48 @@
-import RangeSlider from "../components/form-components/RangeSlider"
-import TagsInput from "../components/form-components/TagsInput"
+import GrowingList from "../components/form-components/GrowingList"
 
 const tq = {
-                label: 'Seasonal',
-                type: 'radio',
-                id: '0-7',
-                options: [
-                    'yes',
-                    'no'
-                ],
-                value: undefined
-            }
-    
+    label: 'Contacts',
+    type: 'growing-list',
+    id: '0-3',
+    min: 1,
+    value: [],
+    fields: [
+        {
+            label: 'First name',
+            type: 'short',
+            id: '0-3-0',
+            value: ''
+        }, {
+            label: 'Last name',
+            type: 'short',
+            id: '0-3-1',
+            value: ''
+        }, {
+            label: 'Email',
+            type: 'email',
+            id: '0-3-2',
+            value: ''
+        }, {
+            label: 'Phone',
+            type: 'short',
+            id: '0-3-3',
+            value: ''
+        }, {
+            label: 'Position',
+            type: 'short',
+            id: '0-3-4',
+            value: ''
+        }
+    ]
+}
+
 export default function Test() {
-    function change(id:string, value:any) {
+    function change(id: string, value: any) {
         console.log(value)
     }
-    
+
     return (
-            <div>
-            <RangeSlider id='123' label='Age Range' steps='85' change={change} validate={() => {return true}} />
+        <div>
             <GrowingList label={tq.label} id={tq.id} fields={tq.fields} change={change} validate={() => true} />
         </div>
     )
