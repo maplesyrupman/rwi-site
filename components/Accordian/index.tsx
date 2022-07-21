ad52000fcf2e744fc13a8cb89cc05b3829b478a5
smudesign <urwinsamantha@gmail.com>
2022-07-21 16:56:26 -0400

    accordian animation

--- components/Accordian/index.tsx
+++ components/Accordian/index.tsx
@@ -2,6 +2,8 @@ import styles from './styles.module.css'
 import { IoIosArrowDown } from 'react-icons/io'
 import { useState } from 'react'
 
+import { motion, AnimatePresence } from 'framer-motion'
+
 type Props = {
     title: string,
     text: string
@@ -28,9 +30,25 @@ export default function Accordian({ title, text }:Props) {
                 <h3>{title}</h3>
                 <IoIosArrowDown className={`${styles.arrow} ${isOpen && styles.rotate}`} />
             </div>
-            <p className={`txtWhite ${styles.accordianTxt} ${isOpen && styles.active}`}>
-                {text}
-            </p>
-        </button>
+            <AnimatePresence initial={false}>
+                {isOpen && (
+                    <motion.div
+                    key='text'
+                    initial='closed'
+                    animate='open'
+                    exit='closed'
+                    variants={{
+                        open: { opacity: 1, height: "auto"},
+                        closed: { opacity: 0, height: 0 }
+                    }}
+                    transition={{ duration: 0.5 }}
+                    >
+                        <p className={`txtWhite ${styles.accordianTxt}`}>
+                            {text}
+                        </p>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </button> 
     )
 }
